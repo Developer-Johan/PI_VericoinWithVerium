86a3b6b82462b996c190d7720b28027d07969d3d
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-18 19:49:56 +0100

    Split initial script vs startupscript

--- initialscript.sh
+++ initialscript.sh
@@ -3,53 +3,6 @@
 cd ~
 clear
 
-while true; do
-  read -p "Do you wish to install/update the needed software? " yn
-    case $yn in
-        [Yy]* ) softwareinstall=true; break;;
-        [Nn]* ) softwareinstall=false; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-while true; do
-  read -p "Do you wish to install docker? " yn
-    case $yn in
-        [Yy]* ) dockerinstall=true; break;;
-        [Nn]* ) dockerinstall=false; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-while true; do
-  read -p "Do you wish to install/update veriumminer? " yn
-    case $yn in
-        [Yy]* ) veriumminerinstall=true; break;;
-        [Nn]* ) veriumminerinstall=false; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-while true; do
-  read -p "Do you wish to install/update vericoin? " yn
-    case $yn in
-        [Yy]* ) vericoininstall=true; break;;
-        [Nn]* ) vericoininstall=false; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-while true; do
-  read -p "Do you wish to install/update verium? " yn
-    case $yn in
-        [Yy]* ) veriuminstall=true; break;;
-        [Nn]* ) veriuminstall=false; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-#------------------------------------------------------------------#
-
 #Install git
 printf "\e[32mInstall git\e[0m\n"
 sudo apt -y install git
@@ -77,47 +30,4 @@ cd ~/git/pi_vericoinwithverium/
 
 #------------------------------------------------------------------#
 
-#Software install
-if [ $softwareinstall = true ]
-then
-    #Run update
-    ./install/update.sh
-
-    #Run installeprogramms
-    ./install/installeprogramms.sh
-fi
-
-#Docker install
-if [ $dockerinstall = true ]
-then
-    #Runinstalldocker
-    install/installdocker.sh
-fi
-
-#veriumminer
-if [ $veriumminerinstall = true ]
-then
-    cd veriumminer
-    ./buildnewdockerimage.sh
-    ./runcontainer.sh
-    cd ..
-fi
-
-#vericoin
-if [ $vericoininstall = true ]
-then
-    cd vericoin
-    ./buildnewdockerimage.sh
-    ./runcontainer.sh
-    cd ..
-fi
-
-#verium
-if [ $veriuminstall = true ]
-then
-    cd verium
-    ./buildnewdockerimage.sh
-    ./runcontainer.sh
-    cd ..
-fi
-
+./startupscript.sh
