64fff07bd295823813f1570783618822fe0e78d5
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-19 20:33:08 +0100

    Forgot volumes/

--- vericoin/setuproothome.sh
+++ vericoin/setuproothome.sh
@@ -14,28 +14,28 @@ then
 	if [ ! -d "/var/lib/docker/volumes/vericoin_root_home/_data/" ] 
 	then
 		printf "\e[32mSetting rights right\e[0m\n"
-		sudo chown $USER /var/lib/docker/vericoin_root_home/_data/ -R
+		sudo chown $USER /var/lib/docker/volumes/vericoin_root_home/_data/ -R
 	fi
 
 	printf "\e[32mGetting vericoin.conf\e[0m\n"
-	cp data/vericoin.conf /var/lib/docker/vericoin_root_home/_data/.vericoin/
+	cp data/vericoin.conf /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/
 
 	printf "\e[32mGetting peers.dat\e[0m\n"
-	cp data/peers.dat /var/lib/docker/vericoin_root_home/_data/.vericoin/
+	cp data/peers.dat /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/
 
 	printf "\e[32mGetting bootstrap.zip\e[0m\n"
-	wget https://pivericoin.blob.core.windows.net/pivericoin/bootstrap.zip -O /var/lib/docker/vericoin_root_home/_data/.vericoin/bootstrap.zip
+	wget https://pivericoin.blob.core.windows.net/pivericoin/bootstrap.zip -O /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/bootstrap.zip
 	printf "\e[32mUnzipping bootstrap.zip\e[0m\n"
-	unzip -q /var/lib/docker/vericoin_root_home/_data/.vericoin/bootstrap.zip
+	unzip -q /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/bootstrap.zip
 
 	printf "\e[32mCopy content bootstrap\e[0m\n"
-	mv /var/lib/docker/vericoin_root_home/_data/.vericoin/bootstrap/* /var/lib/docker/vericoin_root_home/_data/.vericoin/
+	mv /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/bootstrap/* /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/
 	
 	printf "\e[32mCopy directory bootstrap\e[0m\n"
-	rm /var/lib/docker/vericoin_root_home/_data/.vericoin/bootstrap/ -r
+	rm /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/bootstrap/ -r
 	
 	printf "\e[32mCopy file bootstrap.zip\e[0m\n"
-	rm /var/lib/docker/vericoin_root_home/_data/.vericoin/bootstrap.zip 
+	rm /var/lib/docker/volumes/vericoin_root_home/_data/.vericoin/bootstrap.zip 
 
 	printf "Creating scripts directory\n"
 	mkdir /var/lib/docker/volumes/vericoin_root_home/_data/scripts/
