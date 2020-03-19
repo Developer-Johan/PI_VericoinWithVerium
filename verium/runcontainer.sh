bed2e2d2ad8b19d0d27e26fd3176b3185cbcbe2c
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-19 13:35:32 +0100

    sudo forgotten with run

--- verium/runcontainer.sh
+++ verium/runcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rm verium -f
+sudo docker rm verium -f
 sudo docker run -d --restart=always --mount source=verium_root_home,target=/root --cpu-shares 512 --name verium verium /root/scripts/startup.sh
--- veriumminer/runcontainer.sh
+++ veriumminer/runcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rm veriumminer -f
+sudo docker rm veriumminer -f
 sudo docker run -d --restart=always --mount source=veriumminer_root_home,target=/root --cpu-shares 2 --name veriumminer veriumminer /root/scripts/startup.sh
