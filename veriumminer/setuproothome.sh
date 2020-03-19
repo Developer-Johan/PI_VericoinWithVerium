a0e24918b505286fd44f9443e7f0c206ac7945dc
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-19 16:11:58 +0100

    Setup roothome right

--- veriumminer/setuproothome.sh
+++ veriumminer/setuproothome.sh
@@ -16,4 +16,13 @@ then
 		printf "\e[32mSetting rights right\e[0m\n"
 		sudo chown $USER /var/lib/docker/veriumminer_root_home/_data/ -R
 	fi
+
+	printf "\e[32mCreating scripts directory\e[0m\n"
+	mkdir -p /var/lib/docker/volumes/veriumminer_root_home/_data/scripts/
+
+	printf "\e[32mCopy startup script\e[0m\n"
+	cp scripts/startup.sh /var/lib/docker/volumes/veriumminer_root_home/_data/scripts/
+	
+	printf "\e[32mCreating scripts directory\e[0m\n"
+	sudo chmod +x /var/lib/docker/volumes/veriumminer_root_home/_data/scripts/startup.sh
 fi
