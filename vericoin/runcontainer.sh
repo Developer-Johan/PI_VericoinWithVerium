47f53ca7465dc65b97dba703900e52105d33263d
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-20 15:12:48 +0100

    Docker container with port forwarding

--- vericoin/runcontainer.sh
+++ vericoin/runcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker rm vericoin -f
-sudo docker run -d --restart=always --mount source=vericoin_root_home,target=/root --name vericoin vericoin /root/scripts/startup.sh
+sudo docker run -d --restart=always --mount source=vericoin_root_home,target=/root -p 58684:58684 --name vericoin vericoin /root/scripts/startup.sh
--- verium/runcontainer.sh
+++ verium/runcontainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker rm verium -f
-sudo docker run -d --restart=always --mount source=verium_root_home,target=/root --cpu-shares 512 --name verium verium /root/scripts/startup.sh
+sudo docker run -d --restart=always --mount source=verium_root_home,target=/root -p 36988:36988 --cpu-shares 512 --name verium verium /root/scripts/startup.sh
