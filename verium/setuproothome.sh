df28377ce0d562cf11ed3c1289a1ea2479c4a04b
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-20 08:24:47 +0100

    Volume is wrong

--- verium/setuproothome.sh
+++ verium/setuproothome.sh
@@ -9,16 +9,16 @@ fi
 if [ ! -d "/var/lib/docker/volumes/verium_root_home/_data/" ] 
 then
 	printf "\e[32mCreating docker volume for verium\e[0m\n"
-	mkdir -p /var/lib/docker/volumes/verium_root_home/_data/.verium/
+	mkdir /var/lib/docker/volumes/verium_root_home/_data/.verium/
 
 	if [ ! -d "/var/lib/docker/volumes/verium_root_home/_data/.verium/" ] 
 	then
 		printf "\e[32mSetting rights right\e[0m\n"
-		sudo chown $USER /var/lib/docker/volume/verium_root_home/_data/ -R
+		sudo chown $USER /var/lib/docker/volumes/verium_root_home/_data/ -R
 	fi
 
 	printf "\e[32mGetting verium.conf\e[0m\n"
-	cp data/verium.conf /var/lib/docker/volume/verium_root_home/_data/.verium/
+	cp data/verium.conf /var/lib/docker/volumes/verium_root_home/_data/.verium/
 
 	printf "Creating scripts directory\n"
 	mkdir /var/lib/docker/volumes/verium_root_home/_data/scripts/
