9eea87ef70c1ebbc441af3df7b4c5c12ae6762c5
Johan de Vries <johan.de.vries@mstack.nl>
2020-03-20 23:16:30 +0100

    Small fix

--- vericoin/scripts/vericoind_stake_start.sh
+++ vericoin/scripts/vericoind_stake_start.sh
@@ -2,6 +2,6 @@
 
 printf "Start staking\n";
 
-vericoind walletpassphrase whahanotgonnasharemypass 5 9999999 true
+vericoind walletpassphrase whahanotgonnasharemypass 9999999 true
 
 printf "Finished staking\n";
